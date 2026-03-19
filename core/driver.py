21d768bd7315f48f77929d649894972601dbcf8c
DivyaJeyashree <divya.jeyashree@cogniwide.com>
2026-03-19 18:26:51 +0530

    Update driver.py

--- core/driver.py
+++ core/driver.py
@@ -1,17 +1,18 @@
-import os
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 
 def get_driver():
     options = UiAutomator2Options()
-    
-    options.app = os.path.abspath("app.apk")
-    options.set_capability("platformName", "Android")
-    options.set_capability("automationName", "UiAutomator2")
-    options.set_capability("deviceName", "emulator-5554")
-    options.set_capability("appPackage", "com.whatsapp")
-    options.set_capability("appActivity", "com.whatsapp.Main")
-    options.set_capability("noReset", True)
-    options.set_capability("autoGrantPermissions", True)
+
+    options.platform_name = "Android"
+    options.device_name = "emulator-5554"
+
+    # 🔥 IMPORTANT: USE APK DIRECTLY
+    options.app = "app.apk"
+
+    options.automation_name = "UiAutomator2"
+    options.no_reset = True
+    options.auto_grant_permissions = True
+
     driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
     return driver
