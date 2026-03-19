7302beb2863f1b8aea80a4ac2ac487f81ecfcdf6
DivyaJeyashree <divya.jeyashree@cogniwide.com>
2026-03-19 10:19:44 +0530

    Update test_user_flow.py

--- tests/api/test_user_flow.py
+++ tests/api/test_user_flow.py
@@ -7,15 +7,13 @@ from utils.csv_reader import read_csv
 def test_user_flow(user):
     login_response = login_api(user["username"], user["password"])
     print("Login Response:", login_response.text)
-
+    if user["username"] == "wronguser":
+        assert login_response.status_code == 400
+        return
     assert login_response.status_code == 200
-
     token = get_token(login_response)
     print("Token:", token)
-
     assert token is not None
-
     profile_response = get_user_profile(token)
     print("Profile Response:", profile_response.text)
-
     assert profile_response.status_code == 200
