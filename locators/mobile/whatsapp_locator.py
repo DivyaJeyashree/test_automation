2ef4e110f0f422a9bcf27c73ab271042e2e3ee87
DivyaJeyashree <divya.jeyashree@cogniwide.com>
2026-03-19 22:05:34 +0530

    Update whatsapp_locator.py

--- locators/mobile/whatsapp_locator.py
+++ locators/mobile/whatsapp_locator.py
@@ -1,16 +1,12 @@
 from appium.webdriver.common.appiumby import AppiumBy
 
-class WhatsAppLocators:
-    UPDATES_TAB = (AppiumBy.XPATH, "//android.widget.FrameLayout[@content-desc='Updates']")
-    CALLS_TAB = (AppiumBy.XPATH, "//android.widget.FrameLayout[@content-desc='Calls']")
-    CHATS_TAB = (AppiumBy.XPATH, "//android.widget.FrameLayout[@content-desc='Chats']")
-    SEARCH_BAR = (AppiumBy.XPATH, "//android.widget.FrameLayout[@content-desc='Ask Meta AI or Search']")
-    
-    UPDATES_SEARCH_ICON = (AppiumBy.XPATH, "//android.widget.ImageButton[@content-desc='Search']")
-    SEARCH_INPUT = (AppiumBy.XPATH, "//android.widget.EditText")
-    MESSAGE_INPUT   = (AppiumBy.XPATH, "//android.widget.EditText[@text='Message']")
-    MESSAGE_INPUT_2 = (AppiumBy.XPATH, "//android.widget.EditText[@hint='Message']")
-    MESSAGE_INPUT_3 = (AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Message")')
-    MESSAGE_INPUT_4 = (AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.EditText")')
+class AppLocators:
+    # Generic clickable elements
+    FIRST_BUTTON = (AppiumBy.XPATH, "(//android.widget.Button)[1]")
+    SECOND_BUTTON = (AppiumBy.XPATH, "(//android.widget.Button)[2]")
 
-    SEND_BUTTON = (AppiumBy.XPATH, "//android.widget.ImageButton[@content-desc='Send']")
+    # Generic text / input
+    TEXT_FIELD = (AppiumBy.CLASS_NAME, "android.widget.EditText")
+
+    # Generic screen validation
+    ANY_TEXT = (AppiumBy.CLASS_NAME, "android.widget.TextView")
