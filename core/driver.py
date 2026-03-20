0f869c296ccc5fc649ffb9d63b7f36f978b58d53
DivyaJeyashree <divya.jeyashree@cogniwide.com>
2026-03-20 08:23:21 +0530

    Update driver.py

--- core/driver.py
+++ core/driver.py
@@ -2,21 +2,21 @@ import os
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 
+
 def get_driver():
     options = UiAutomator2Options()
 
-    # ✅ Try local Downloads first
+    # ✅ APK path (local + GitHub)
     local_apk = os.path.expanduser("~/Downloads/app-release-4-3-26.apk")
     repo_apk = os.path.abspath("app.apk")
 
-    if os.path.exists(local_apk):
-        options.app = local_apk
-    else:
-        options.app = repo_apk   # for GitHub Actions
+    options.app = local_apk if os.path.exists(local_apk) else repo_apk
 
+    # ✅ Emulator capabilities
     options.set_capability("platformName", "Android")
     options.set_capability("automationName", "UiAutomator2")
     options.set_capability("deviceName", "emulator-5554")
+
     options.set_capability("autoGrantPermissions", True)
     options.set_capability("noReset", False)
 
