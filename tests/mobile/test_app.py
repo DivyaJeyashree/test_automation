7770a9f910f9a3180b0914be4a052e02ffc35fa3
DivyaJeyashree <divya.jeyashree@cogniwide.com>
2026-03-20 08:33:32 +0530

    Update and rename test_whatspp.py to test_app.py

--- tests/mobile/test_app.py
+++ tests/mobile/test_app.py
@@ -1,4 +1,3 @@
-import pytest
 import allure
 from core.driver import get_driver
 from pages.mobile.app_page import AppPage
@@ -6,6 +5,7 @@ from pages.mobile.app_page import AppPage
 
 @allure.epic("APK Automation")
 @allure.feature("Basic Testing")
+
 
 # ✅ Test 1
 def test_click_first_button():
@@ -20,10 +20,11 @@ def test_click_first_button():
 
 
 # ✅ Test 2
-def test_click_second_button():
+def test_click_second_button_with_text():
     driver = get_driver()
     page = AppPage(driver)
 
+    page.enter_text("Hello Automation")
     page.click_second_button()
 
     assert page.is_screen_loaded()
