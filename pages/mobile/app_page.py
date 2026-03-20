7a3ab9059b82d5577d1446f1225408390e6acb53
DivyaJeyashree <divya.jeyashree@cogniwide.com>
2026-03-20 08:32:46 +0530

    Update and rename whatspp_page.py to app_page.py

--- pages/mobile/app_page.py
+++ pages/mobile/app_page.py
@@ -2,11 +2,12 @@ from locators.mobile.app_locators import AppLocators
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class AppPage:
 
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 15)
+        self.wait = WebDriverWait(driver, 20)
 
     def click_first_button(self):
         element = self.wait.until(
@@ -19,6 +20,12 @@ class AppPage:
             EC.element_to_be_clickable(AppLocators.SECOND_BUTTON)
         )
         element.click()
+
+    def enter_text(self, text):
+        field = self.wait.until(
+            EC.presence_of_element_located(AppLocators.TEXT_FIELD)
+        )
+        field.send_keys(text)
 
     def is_screen_loaded(self):
         elements = self.driver.find_elements(*AppLocators.ANY_TEXT)
