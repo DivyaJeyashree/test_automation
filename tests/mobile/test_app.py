7a97089b2ee29c7633bcafb1415db32cdb9e0110
DivyaJeyashree <divya.jeyashree@cogniwide.com>
2026-03-20 10:48:13 +0530

    Update test_app.py

--- tests/mobile/test_app.py
+++ tests/mobile/test_app.py
@@ -1,20 +1,29 @@
-import os
 import allure
 from core.driver import get_driver
 from pages.mobile.app_page import AppPage
 
 
 @allure.epic("APK Automation")
-@allure.feature("Login Testing")
+@allure.feature("Basic Testing")
 
 
-def test_login():
+def test_click_first_button():
     driver = get_driver()
     page = AppPage(driver)
 
-    password = os.getenv("APP_PASSWORD", "Test@123")
+    page.click_first_button()
 
-    page.login(password)
+    assert page.is_screen_loaded()
+
+    driver.quit()
+
+
+def test_click_second_button():
+    driver = get_driver()
+    page = AppPage(driver)
+
+    page.enter_text("Hello")
+    page.click_second_button()
 
     assert page.is_screen_loaded()
 
